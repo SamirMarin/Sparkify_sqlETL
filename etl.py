b8d5ffb114e7c2d02b84f66219164cbf59a9750b
Samir Marin <marin.samir@gmail.com>
2019-04-10 22:57:15 -0700

    Refactor selection of song and artist dataFrame

--- etl.py
+++ etl.py
@@ -4,26 +4,16 @@ import psycopg2
 import pandas as pd
 from sql_queries import *
 
-
-def song_data_tuple(df):
-    test = (df.song_id.values[0], df.title.values[0], df.artist_id.values[0], int(df.year.values[0]), df.duration.values[0])
-    print(test)
-    return test
-
-def artist_data_tuple(df):
-    return (df.artist_id.values[0], df.artist_name.values[0], df.artist_location.values[0], df.artist_latitude.values[0], df.artist_longitude.values[0])
-
-
 def process_song_file(cur, filepath):
     # open song file
     df = pd.read_json(filepath, lines=True)
 
     # insert song record
-    song_data = song_data_tuple(df)
+    song_data = list(df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0])
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = artist_data_tuple(df)
+    artist_data = list(df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0])
     cur.execute(artist_table_insert, artist_data)
 
 
@@ -92,7 +82,7 @@ def main():
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
-    #process_data(cur, conn, filepath='data/song_data', func=process_song_file)
+    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
 
     conn.close()
